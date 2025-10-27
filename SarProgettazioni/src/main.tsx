1f555fd391e6e4493a4781d9243527471053840c
simoneRighi <simone.righi@marconirovereto.it>
2025-10-27 16:18:08 +0100

    daContinuaACasa

--- SarProgettazioni/src/main.tsx
+++ SarProgettazioni/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/navbar/Navbar.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <header>
+      <Navbar />
+    </header>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
